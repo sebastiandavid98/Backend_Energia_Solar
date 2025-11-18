32a7c1a0bbbdf07b1101b7322050e7fed06a53e1
sebastiandavid98 <sebastian.david@campusucc.edu.co>
2025-11-18 15:48:13 -0500

    AAAfregar PingControlle para pruebas

---
+++ src/main/java/com/plataformaenergia/backend/controller/PingController.java
@@ -0,0 +1,13 @@
+package com.plataformaenergia.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/api/ping")
+    public String ping() {
+        return "Backend funcionando OK ✔️";
+    }
+}
